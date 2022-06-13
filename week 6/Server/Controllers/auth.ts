b1a5f9f82d0071b4f83849e260f433f9ff834b9d
Rick L <richyray24@gmail.com>
2022-06-13 11:11:32 -0400

    prevented user from loging and registering if already did

--- week 6/Server/Controllers/auth.ts
+++ week 6/Server/Controllers/auth.ts
@@ -8,12 +8,21 @@ import { UserDisplayName } from '../Util';
 //display functions
 export function DisplayLoginPage(req: express.Request, res: express.Response, next: express.NextFunction)
 {
-    res.render('index', {title: 'Login', page: 'login', messages: req.flash('loginMessage'), displayName: UserDisplayName(req)});
+    if(!req.user){
+       return  res.render('index', {title: 'Login', page: 'login', messages: req.flash('loginMessage'), displayName: UserDisplayName(req)});
+
+    }
+    return res.redirect('/movie-list');
 }
 
 export function DisplayRegisterPage(req: express.Request, res: express.Response, next: express.NextFunction) 
 {
-    res.render('index', {title: 'Register', page: 'register', messages: req.flash('registerMessage'), displayName:UserDisplayName(req)});
+    if(!req.user){
+       return res.render('index', {title: 'Register', page: 'register', messages: req.flash('registerMessage'), displayName:UserDisplayName(req)});
+
+    }
+    return res.redirect('/movie-list');
+
 }
 
 //processing functions
